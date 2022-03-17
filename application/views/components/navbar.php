61d7ac2ccea3f3d966d5a99ba344b6c9347cba60
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-03-17 16:30:05 +0700

    last for today

---
+++ application/views/components/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Auction</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item <?= $title === "Home" ? 'active' : ''; ?>">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item <?= $title === "About" ? 'active' : ''; ?>">
+                    <a class="nav-link" href="/pages/about">About</a>
+                </li>
+                <li class="nav-item <?= $title === "Review" ? 'active' : ''; ?>">
+                    <a class="nav-link" href="/pages/review">Review</a>
+                </li>
+            </ul>
+            <div>
+                <a href="/login" class="btn btn-primary" type="button">LOGIN</a>
+                <a href="/register" class="btn btn-danger ml-3" type="button">REGISTER</a>
+            </div>
+        </div>
+    </div>
+</nav>
