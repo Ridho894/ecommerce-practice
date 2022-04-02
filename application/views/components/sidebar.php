c5362225621f3b7e77fb30c179cadbe8051da711
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-04-02 18:13:27 +0700

    routing sidebar

--- application/views/components/sidebar.php
+++ application/views/components/sidebar.php
@@ -8,30 +8,30 @@
             <a href="index.html">RD</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="nav-item dropdown active">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            <li class="nav-item <?= $title === "Dashboard" ? "active" : ""; ?>">
+                <a href="<?= base_url(); ?>index.php/pages/dashboard" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Category</span></a>
+            <li class="nav-item <?= $title === "Category" ? "active" : ""; ?>">
+                <a href="<?= base_url(); ?>index.php/pages/category" class="nav-link"><i class="fas fa-columns"></i> <span>Category</span></a>
             </li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-paper-plane"></i> <span>Jasa Pengiriman</span></a>
                 <ul class="dropdown-menu">
                     <li>
-                        <a class="nav-link" href="bootstrap-alert.html">Alert</a>
+                        <a class="nav-link" href="bootstrap-alert.html">Jnt</a>
                     </li>
                     <li>
-                        <a class="nav-link" href="bootstrap-badge.html">Badge</a>
+                        <a class="nav-link" href="bootstrap-badge.html">Jne</a>
                     </li>
                     <li>
-                        <a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a>
+                        <a class="nav-link" href="bootstrap-breadcrumb.html">SiCepat</a>
                     </li>
                 </ul>
             </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-paper-plane"></i> <span>Member</span></a>
+            <li class="nav-item <?= $title === "Member" ? "active" : ""; ?>">
+                <a href="<?= base_url(); ?>index.php/pages/member" class="nav-link"><i class="fas fa-paper-plane"></i> <span>Member</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= $title === "Dashboard" ? "active" : ""; ?>">
                 <a href="#" class="nav-link"><i class="fas fa-th-large"></i> <span>Toko</span></a>
             </li>
         </ul>
